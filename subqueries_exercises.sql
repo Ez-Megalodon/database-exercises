b6ca92c046f139f477ab48981126c3946521781a
Ezra Marama <ezra.marama@gmail.com>
2023-05-05 11:46:10 -0500

    completed MySQL 'Subqueries' exercise

---
+++ subqueries_exercises.sql
@@ -0,0 +1,54 @@
+use employees;
+
+select first_name, last_name
+from employees
+    where hire_date in (
+        select hire_date
+        from employees
+        where emp_no = 101010
+        );
+
+select title
+from titles
+    where emp_no in (
+        select emp_no
+        from employees
+        where first_name = 'Aamod'
+        );
+
+select first_name, last_name
+from employees
+    where emp_no in (
+        select emp_no
+        from dept_manager
+        where to_date like '9%'
+              )
+    and (gender = 'F');
+
+# BONUS 1
+select dept_name
+from departments
+    where dept_no in (
+        select dept_no
+        from dept_manager
+        where emp_no in (
+            select emp_no
+            from employees
+            where gender = 'F'
+            )
+        and to_date like '9%'
+        order by dept_name
+              );
+
+# BONUS 2
+select first_name, last_name
+from employees
+    where emp_no in (
+        select emp_no
+        from salaries
+        where salary in (
+            select MAX(salary)
+            from salaries
+            )
+        );
+
